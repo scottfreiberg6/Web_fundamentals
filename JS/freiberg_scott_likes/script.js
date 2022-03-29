4aeb3c7fa7ae85bc04dbb2edecdb694974f5f1cf
scottfreiberg6 <scott.freiberg6@gmail.com>
2022-03-29 17:48:48 -0400

    changes

---
+++ JS/freiberg_scott_likes/script.js
@@ -0,0 +1,21 @@
+function likes () {
+
+var update =document.querySelector("#button")
+console.log(update.innerHTML)
+update.innerHTML= parseInt (update.innerHTML) + 1
+}
+
+function likes1 () {
+
+    var update =document.querySelector("#button1")
+    console.log(update.innerHTML)
+    update.innerHTML= parseInt (update.innerHTML) + 1
+    }
+
+    function likes2 () {
+
+        var update =document.querySelector("#button2")
+        console.log(update.innerHTML)
+        update.innerHTML= parseInt (update.innerHTML) + 1
+        }
+    
